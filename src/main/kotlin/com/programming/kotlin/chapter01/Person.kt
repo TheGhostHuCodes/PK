5b70246216a0c81943010a043a45c181485bc4ac
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-11-22 10:30:04 -0800

    Add argument validation to Person construction

--- src/main/kotlin/com/programming/kotlin/chapter01/Person.kt
+++ src/main/kotlin/com/programming/kotlin/chapter01/Person.kt
@@ -1,3 +1,11 @@
 package com.programming.kotlin.chapter01
 
-class Person constructor(val firstName: String, val lastName: String, val age: Int?) {}
+class Person constructor(val firstName: String, val lastName: String, val age: Int?) {
+    init {
+        require(firstName.trim().isNotEmpty()) {"Invalid firstName argument."}
+        require(lastName.trim().isNotEmpty()) {"Invalid lastName argument."}
+        if (age != null) {
+            require(age in 0..149) {"Invalid age argument."}
+        }
+    }
+}
